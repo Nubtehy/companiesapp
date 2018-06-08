1f273fe2f751188e602a40b5078f72ef265e9811
Naysan <momka@mail.ru>
2018-06-08 17:19:08 +0200

    add fixes

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 
 app.get('/', function (req, res) {
-  res.send('index.html')
+  res.sendFile('index.html')
 });
 // Main route sends our HTML file
 
