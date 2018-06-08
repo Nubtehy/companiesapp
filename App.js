faf65fadf257935e86ff0b5a69b619ab838fd292
Naysan <momka@mail.ru>
2018-06-08 17:08:02 +0200

    change ports config

--- App.js
+++ App.js
@@ -117,6 +117,7 @@ app.delete('/company/:id', function (req, res) {
 app.get('/company/:id', function (req, res, next) {
   res.send('company');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port: ' + port);
 });
