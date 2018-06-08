04b914f3d2a8d93a0330ec036b3533beac41ed0d
Naysan <momka@mail.ru>
2018-06-08 17:47:45 +0200

    add build

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ connection.query('CREATE DATABASE IF NOT EXISTS '+config.db.name, function (err)
   });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -118,7 +118,7 @@ app.get('/company/:id', function (req, res, next) {
   res.send('company');
 });
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname,'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 var port = process.env.PORT || 3000;
