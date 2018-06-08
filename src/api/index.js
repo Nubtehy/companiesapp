0b22b6744494835c733fddce0a298126b43fa750
Naysan <momka@mail.ru>
2018-06-08 15:32:52 +0200

    add change to apiPrefex

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const  apiPrefix="http://localhost:3000";
+const  apiPrefix="mysql://dog4wtomi2vl5w81:bkir20kv3ac0rr1q@n7qmaptgs6baip9z.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/poqjubfg0a4kskou";
 
 export default {
   parentCompanies(data){
